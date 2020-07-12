71d197ad87d012a34a7bd1c5d68dd21a611af250
Roland Schuller <roland@it-open.at>
2020-07-12 16:53:45 +0200

    First Login

--- src/main/java/at/itopen/mapillary/Main.java
+++ src/main/java/at/itopen/mapillary/Main.java
@@ -15,7 +15,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Mapillary m = new Mapillary();
+        m.startOAuthServer(null);
+        m.startOAuth(Mapillary.SCOPE.USER_READ);
+        m.waitforAccess(50);
     }
 
 }
--- src/main/java/at/itopen/mapillary/Mapillary.java
+++ src/main/java/at/itopen/mapillary/Mapillary.java
@@ -5,17 +5,158 @@
  */
 package at.itopen.mapillary;
 
+import at.itopen.simplerest.RestHttpServer;
+import at.itopen.simplerest.client.RestClient;
+import at.itopen.simplerest.client.RestResponse;
+import at.itopen.simplerest.conversion.ContentType;
+import at.itopen.simplerest.conversion.Conversion;
+import at.itopen.simplerest.endpoints.GetEndpoint;
+import java.awt.Desktop;
+import java.io.IOException;
+import static java.lang.Thread.sleep;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author roland
  */
 public class Mapillary {
 
-    private static String rootEndpoint = "https://a.mapillary.com/3";
-    private String clientID;
+    private static String rootEndpoint = "http://a.mapillary.com/v3";
+    private static String ClientID = "UzZRbjZEUm1jNGFsNi1CS3g3RjNydzpmYjM2MDJiNDA1ZGE1MDYw";
+    private static String ClientSecret = "ZGE5MTMyOGZjODQzNjM1ZDdhMTVjMDEwYzJjNjIyYWQ=";
+    private static String RedirectUrl = "http://localhost:9876/token";
 
-    public Mapillary(String clientID) {
-        this.clientID = clientID;
+    private RestHttpServer httpserver;
+    private String access_token = null;
+    private Boolean access = null;
+
+    public Mapillary() {
+
+    }
+
+    public void startOAuthServer(Integer port) {
+        if (httpserver != null) {
+            return;
+        }
+
+        if (port == null) {
+            port = 9876;
+        }
+        httpserver = new RestHttpServer(port);
+        httpserver.getRootEndpoint().addRestEndpoint(new GetEndpoint("token") {
+            @Override
+            public void Call(Conversion conversion, Map<String, String> params) {
+                access_token = conversion.getRequest().getParam("access_token");
+                access = access_token != null;
+                conversion.getResponse().setContentType(ContentType.HTML);
+                conversion.getResponse().setData("<html><h1>READY</h1></html>");
+            }
+        });
+        final RestHttpServer h = httpserver;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    h.run();
+                } catch (Exception ex) {
+                    Logger.getLogger(Mapillary.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }).start();
+
+    }
+
+    public void waitforAccess(int timeoutSeconds) {
+        timeoutSeconds = timeoutSeconds * 10;
+        while (access == null) {
+            try {
+                sleep(100);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Mapillary.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            timeoutSeconds--;
+            if (timeoutSeconds == 0) {
+                return;
+            }
+        }
+        if (Boolean.TRUE.equals(access)) {
+            httpserver.shutdown();
+            RestClient rc = new RestClient(rootEndpoint + "/oauth/token", RestClient.REST_METHOD.POST);
+            rc.setMultipart(false);
+            rc.authKey(access_token);
+            rc.setParameter("client_id", ClientID);
+            rc.setParameter("client_secret", ClientSecret);
+            rc.setParameter("redirect_uri", RedirectUrl);
+            rc.setParameter("grant_type", "bearer");
+            rc.setParameter("code", access_token);
+            RestResponse rr = rc.toSingle(true);
+            System.out.println(rr.getDataAsString());
+        }
+
+    }
+
+    public Boolean hasAccess() {
+        return access;
+    }
+
+    public static enum SCOPE {
+        USER_READ, USER_WRITE, USER_EMAIL, PUBLIC_WRITE, PUBLI_UPLOAD, PRIVATE_READ, PRIVATE_WRITE, PRIVATE_UPLOAD
+    };
+
+    public void startOAuth(SCOPE... scopes) {
+
+        StringBuilder url = new StringBuilder();
+        url.append("https://www.mapillary.com/connect?");
+        url.append("client_id=").append(ClientID);
+        url.append("&" + "response_type=" + "token");
+        url.append("&redirect_uri=").append(RedirectUrl.replaceAll("/", "%2F"));
+
+        StringBuilder scope = new StringBuilder();
+        for (SCOPE scp : scopes) {
+            String s = scp.name().toLowerCase();
+            s = s.replace('_', ':');
+            if (scope.length() > 0) {
+                scope.append("%20");
+            }
+            scope.append(s);
+        }
+        url.append("&scope=").append(scope.toString());
+        try {
+            Desktop.getDesktop().browse(new URI(url.toString()));
+        } catch (URISyntaxException | IOException ex) {
+            Logger.getLogger(Mapillary.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setClientID(String ClientID) {
+        Mapillary.ClientID = ClientID;
+    }
+
+    public static String getClientID() {
+        return ClientID;
+    }
+
+    public static void setRedirectUrl(String RedirectUrl) {
+        Mapillary.RedirectUrl = RedirectUrl;
+    }
+
+    public static String getRedirectUrl() {
+        return RedirectUrl;
+    }
+
+    public static String getClientSecret() {
+        return ClientSecret;
+    }
+
+    public static void setClientSecret(String ClientSecret) {
+        Mapillary.ClientSecret = ClientSecret;
     }
 
 }
